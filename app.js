75bd4a1cada8a748b878413de8ae9ce27f633257
Sado <namehsado@yahoo.com>
2024-12-15 17:25:00 +0100

    fix bug

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 const express = require('express')
 const app = express()
-const routes = require('./routes/tasks')
+const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect')
 require("dotenv").config() 
 const notFound = require('./middlewares/not-found')
 const errorHandlerMiddleware = require('./middlewares/error-handler')
 
-
-app.use('/api/v1/tasks', express.static('./public'))
+//middleware
+app.use( express.static('./public'))
 app.use(express.json())
 
-/*app.get('/', (req, res) => {
+//routes
+app.get('/', (req, res) => {
     console.log('Task Manager App ')
     res.send('welcome')
-})*/
+})
 
-
-
-app.use('/api/v1/tasks', routes)
+app.use('/api/v1/tasks', tasks)
 app.use(notFound)
 app.use(errorHandlerMiddleware)
 
+//server
 const port = process.env.PORT || 5000
 
 const start = async() => {
